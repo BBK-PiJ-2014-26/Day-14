fb09f64a7c7168e0178a68e38681ca79c2246e6f
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-19 21:05:02 +0000

    first commit

---
+++ Hailstone.java
@@ -0,0 +1,13 @@
+import java.awt.List;
+
+public class Hailstone {
+
+	public List<Integer> returnHailstoneNumbers(int x) {
+		if (n % 2 == 0) {
+			n = n / 2;
+
+		} else {
+			n = (3 * n) + 1;
+
+
+}
