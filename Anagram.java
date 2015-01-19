3874c174b8d03b45860473c0df9adced19cb413b
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-19 20:54:01 +0000

    almost there

--- Anagram.java
+++ Anagram.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
+
 public class Anagram {
 
-	public static void listAnagrams(char firstLetter, String remainingChar) {
-		List<String> precalculated = new List();
-		for(int i = 0; i > (remainingChar.length + 1); i++) {
-			precalculated.add(firstLetter);
+	public static List<String> getAnagrams(String s) {
+		List<String> result = new ArrayList<String>();
+		if (s.length() == 1) {
+			result.add(s);
+			return result;
+		} else {
+			for(int i = 0; i < s.length(); i++) {
+				String firstLetter = "" + s.charAt(i);
+				String rest = s.substring(0, i) + s.subString(i + 1, s.length());
+				List<String> subAnagrams = getAnagrams(rest);
+				for(int i = 0; i < subAnagrams; i++) {
+				String x = firstLetter + subAnagrams.getItem[i];
+					result.add(x);
+				}
+			}
+			return result;
 		}
-		for(int i = 0;
+	}
+
+	public static void main(String[] args) {
+		String s = "abs";
+		List<String> anagramList = ArrayList<String>();
+		anagramList = Anagram.getAnagrams(s);
+		for (int i = 0; i < anagramList.size(); i++) {
+			System.out.println(anagramList.getItem());
+		}
+	}
 }
